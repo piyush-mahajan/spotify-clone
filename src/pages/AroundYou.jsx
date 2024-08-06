31bb640a1221a338dde9994beea02cfb111055bb
Piyush Mahajan <98900169+piyush-mahajan@users.noreply.github.com>
2024-08-06 23:44:04 +0530

    Update AroundYou.jsx

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -13,7 +13,7 @@ const CountryTracks = () => {
 
   useEffect(() => {
     axios
-      .get(`https://geo.ipify.org/api/v2/country?apiKey=${import.meta.env.VITE_GEO_API_KEY}`)
+      .get(`https://geo.ipify.org/api/v2/country?apiKey=at_dcJvMzJpjb0AynBgE3y5DDK9hpduo`)
       .then((res) => setCountry(res?.data?.location.country))
       .catch((err) => console.log(err))
       .finally(() => setLoading(false));
