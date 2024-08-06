71667b2aa941083559ae32ff838e0dc6ca2d8683
Piyush Mahajan <98900169+piyush-mahajan@users.noreply.github.com>
2024-08-06 23:43:05 +0530

    Update shazamCore.js

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -5,7 +5,7 @@ export const shazamCoreApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://shazam-core.p.rapidapi.com/',
     prepareHeaders: (headers) => {
-      headers.set('X-RapidAPI-Key', import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY);
+      headers.set('X-RapidAPI-Key', '39a64bc971msh3742f5ced0d9abbp128c75jsnbea68549e32f');
 
       return headers;
     },
